7a5d90ac2991c77e0d3b98ba0662b2c328ac6e07
Pistonight <zhaoyizhu2000@gmail.com>
2023-12-02 20:06:07 -0800

    reorganize headers

---
+++ source/exl/usersetting.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "common.hpp"
+// these should be defined
+#define EXL_DEBUG
+#define EXL_USE_FAKEHEAP
+
+#ifndef EXL_HEAP_SIZE
+#define EXL_HEAP_SIZE 0x5000
+#endif
+
+#ifndef EXL_JIT_SIZE
+#define EXL_JIT_SIZE 0x1000
+#endif
+
+#ifndef EXL_INLINE_POOl_SIZE
+#define EXL_INLINE_POOL_SIZE 0x1000
+#endif
+
+/*
+#define EXL_SUPPORTS_REBOOTPAYLOAD
+*/
+
+namespace exl::setting {
+    /* How large the fake .bss heap will be. */
+    constexpr size_t HeapSize = EXL_HEAP_SIZE;
+
+    /* How large the JIT area will be for hooks. */
+    constexpr size_t JitSize = EXL_JIT_SIZE;
+
+    /* How large the area will be inline hook pool. */
+    constexpr size_t InlinePoolSize = EXL_INLINE_POOL_SIZE;
+
+    /* Sanity checks. */
+    static_assert(ALIGN_UP(JitSize, PAGE_SIZE) == JitSize, "JitSize is not aligned");
+    static_assert(ALIGN_UP(InlinePoolSize, PAGE_SIZE) == JitSize, "InlinePoolSize is not aligned");
+}
